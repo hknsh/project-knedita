05a53252a4414f2945e9f6a43a96e60ba2bfe7e1
CookieDasora <valterliviaamaral@gmail.com>
2024-01-24 20:14:42 +0000

    chore: removed swagger custom css

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,7 +1,6 @@
 import type { SwaggerUiOptions } from "swagger-ui-express";
 
 const swaggerConfig: SwaggerUiOptions = {
-  customCssUrl: "/swagger-ui.css",
   customSiteTitle: "Project Knedita Docs",
   customfavIcon: "/favicon.png",
 };
